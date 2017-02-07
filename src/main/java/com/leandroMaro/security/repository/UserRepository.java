40d70193cbcd95e08ec2c6e3e56ddda289689f56
leandromaro <leandro.maro@globant.com>
2017-02-07 08:41:27 -0300

    Adding security controlers and users

---
+++ src/main/java/com/leandroMaro/security/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.leandroMaro.security.repository;
+
+import com.leandroMaro.models.security.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by stephan on 20.03.16.
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
